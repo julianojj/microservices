52be1269f9dccc04a823abdb4c924f2b29bc89c8
Juliano Silva <cont.juliano@outlook.com>
2022-11-09 22:10:31 -0300

    refactor: added a create user usecase to controller

--- accounts/src/infra/api/controller/create_user_controller.go
+++ accounts/src/infra/api/controller/create_user_controller.go
@@ -22,5 +22,18 @@ func NewCreateUserController(
 func (u *CreateUserController) Handle(w http.ResponseWriter, r *http.Request) {
 	var input usecase.CreateUserInput
 	json.NewDecoder(r.Body).Decode(&input)
-	json.NewEncoder(w).Encode(input)
+	_, err := u.createUser.Execute(input)
+	if err == nil {
+		w.WriteHeader(http.StatusCreated)
+		return
+	}
+	if err.Error() == "invalid email" || err.Error() == "invalid password" {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if err.Error() == "user already exists" {
+		http.Error(w, err.Error(), http.StatusConflict)
+		return
+	}
+	http.Error(w, "internal server error", http.StatusInternalServerError)
 }
