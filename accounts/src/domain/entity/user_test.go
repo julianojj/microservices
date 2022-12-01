c7b84fab3f5518958687bf88410676363d6b9e17
Juliano Silva <cont.juliano@outlook.com>
2022-12-01 01:01:15 +0000

    test: added a new tests to create a new user

--- accounts/src/domain/entity/user_test.go
+++ accounts/src/domain/entity/user_test.go
@@ -7,8 +7,92 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNotShouldCreateUserIfIdIsRequired(t *testing.T) {
+	user, err := NewUser(
+		"",
+		"any_user",
+		"any_username",
+		"any_email@test.com",
+		"any_password",
+	)
+	assert.Nil(t, user)
+	assert.EqualError(t, err, "id is required")
+}
+
+func TestNotShouldCreateUserIfNameIsRequired(t *testing.T) {
+	user, err := NewUser(
+		uuid.NewString(),
+		"",
+		"any_username",
+		"any_email@test.com",
+		"any_password",
+	)
+	assert.Nil(t, user)
+	assert.EqualError(t, err, "name is required")
+}
+
+func TestNotShouldCreateUserIfUsernameIsRequired(t *testing.T) {
+	user, err := NewUser(
+		uuid.NewString(),
+		"any_user",
+		"",
+		"any_email@test.com",
+		"any_password",
+	)
+	assert.Nil(t, user)
+	assert.EqualError(t, err, "username is required")
+}
+
+func TestNotShouldCreateUserIfEmailIsRequired(t *testing.T) {
+	user, err := NewUser(
+		uuid.NewString(),
+		"any_user",
+		"any_username",
+		"",
+		"any_password",
+	)
+	assert.Nil(t, user)
+	assert.EqualError(t, err, "email is required")
+}
+
+func TestNotShouldCreateUserIfPasswordIsRequired(t *testing.T) {
+	user, err := NewUser(
+		uuid.NewString(),
+		"any_user",
+		"any_username",
+		"any_email@test.com",
+		"",
+	)
+	assert.Nil(t, user)
+	assert.EqualError(t, err, "password is required")
+}
+
+func TestNotShouldCreateUserIfInvalidEmail(t *testing.T) {
+	user, err := NewUser(
+		uuid.NewString(),
+		"any_user",
+		"any_username",
+		"any_emailtest.com",
+		"any_password",
+	)
+	assert.Nil(t, user)
+	assert.EqualError(t, err, "invalid email")
+}
+
+func TestNotShouldCreateUserIfInvalidPassword(t *testing.T) {
+	user, err := NewUser(
+		uuid.NewString(),
+		"any_user",
+		"any_username",
+		"any_email@test.com",
+		"12345",
+	)
+	assert.Nil(t, user)
+	assert.EqualError(t, err, "invalid password")
+}
+
 func TestCreateUser(t *testing.T) {
-	user, _ := NewUser(
+	user, err := NewUser(
 		uuid.NewString(),
 		"any_user",
 		"any_username",
@@ -16,26 +100,5 @@ func TestCreateUser(t *testing.T) {
 		"any_password",
 	)
 	assert.Equal(t, user.Name, "any_user")
-}
-
-func TestNotShouldCreateUserIfInvalidEmail(t *testing.T) {
-	_, err := NewUser(
-		uuid.NewString(),
-		"any_user",
-		"any_username",
-		"any_emailtest.com",
-		"any_password",
-	)
-	assert.EqualError(t, err, "invalid email")
-}
-
-func TestNotShouldCreateUserIfInvalidPassword(t *testing.T) {
-	_, err := NewUser(
-		uuid.NewString(),
-		"any_user",
-		"any_username",
-		"any_email@test.com",
-		"12345",
-	)
-	assert.EqualError(t, err, "invalid password")
+	assert.NoError(t, err)
 }
